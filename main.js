2a60532fbc32c587eb38d36a9efca4a011ccf5df
Mark Dumler <90216931+dummark@users.noreply.github.com>
2025-01-05 21:16:55 +0300

    third commit

--- main.js
+++ main.js
@@ -8,6 +8,11 @@ class View {
 		this.searchLine = this.createElement('div', 'search-line');
 		this.searchInput = this.createElement('input', 'search-input');
 		this.searchCounter = this.createElement('span', 'counter');
+		this.addedRepositories = this.createElement(
+			'div',
+			'add-repositories-wrapper'
+		);
+		this.addedRepository = this.createElement('div', 'add-repository');
 		this.searchLine.append(this.searchInput);
 		this.searchLine.append(this.searchCounter);
 
@@ -33,16 +38,46 @@ class View {
 		}
 		return element;
 	}
+
+	createRepository(repositoryData) {
+		const repositoryElement = this.createElement('li', 'repository-prev');
+		repositoryElement.innerHTML = `<span class="repository-prev-name">${repositoryData.name}</span>`;
+		this.repositoriesList.append(repositoryElement);
+	}
 }
+
+const REPOSITORIES_PER_PAGE = 5;
 
 class Search {
 	constructor(view) {
 		this.view = view;
 
-		this.view.searchInput.addEventListener('keyup', this.searchRepositories());
+		this.view.searchInput.addEventListener(
+			'keyup',
+			this.searchRepositories.bind(this)
+		);
+
+		this.view.repositoryElement.addEventListener(
+			'click',
+			this.addRepository.bind(this)
+		);
 	}
 
-	searchRepositories() {}
+	async searchRepositories() {
+		return await fetch(
+			`https://api.github.com/search/repositories?q=${this.view.searchInput.value}&per_page=${REPOSITORIES_PER_PAGE}`
+		).then(res => {
+			if (res.ok) {
+				res.json().then(res => {
+					res.items.forEach(repository =>
+						this.view.createRepository(repository)
+					);
+				});
+			}
+		});
+	}
+
+	addRepository() {}
 }
 
 new Search(new View());
